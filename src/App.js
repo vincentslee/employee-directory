117d188fb2169504791bddbe42442291e211badd
Vincent Lee <911vincentlee@gmail.com>
2020-10-29 19:09:25 -0500

    Reinstalled react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, {useEffect, useState} from "react";
-import Wrapper from "./components/Wrapper";
-import Table from "./components/Header";
-import Main from "./components/Main"
-import {getUsers} from "./API";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-function App(){
-    return (
-        <div className="App">
-            <Wrapper>
-                <Header/>
-                <Main/>
-            </Wrapper>
-        </div>
-    );
+import Wrapper from "./components/Wrapper";
+import Table from "./components/Table";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Table/>
+      </header>
+    </div>
+  );
 }
 
 export default App;
